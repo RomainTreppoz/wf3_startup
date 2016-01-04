c513519103809f60656c6cd916f3d6fbca90c2dc
RomainTreppoz <romaintreppoz@gmail.com>
2016-01-04 17:15:16 +0100

    test conflit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-require_once(__DIR__.'/config/db.php');
+require_once(__DIR__.'/config/dc.php');
 
 // 1. Récupérer la variable en GET : id en checkant qu'elle est bien défini
 // 2. Afficher la variable id. Exemple : http://localhost/php/26/pizza_site/index.php?id=2
